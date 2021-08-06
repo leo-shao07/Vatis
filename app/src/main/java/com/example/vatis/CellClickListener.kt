1368c67f39a72285391fc8a147d845aceb9e6afe
kizune4427 <leo90610@gmail.com>
2021-08-07 01:47:57 +0800

    feat(book): add onCellClistListener for BookmarkItem

--- app/src/main/java/com/example/vatis/CellClickListener.kt
+++ app/src/main/java/com/example/vatis/CellClickListener.kt
@@ -1,8 +1,10 @@
 package com.example.vatis
 
+import com.example.vatis.items.BookmarkItem
 import com.example.vatis.items.MemoSubItem
 
 interface CellClickListener {
-    fun onCellClickListener(data: MemoSubItem)
+    fun onCellClickListener(data: BookmarkItem) {}
+    fun onCellClickListener(data: MemoSubItem) {}
 
 }
